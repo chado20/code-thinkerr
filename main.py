abac4f034ec527feb99c63f04bfecda82aca33bb
chado20 <chahedc677@gmail.com>
2026-04-20 18:37:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -262,4 +262,6 @@ def delete_result(result_id: int):
 # ===============================
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    # Render يعطينا البورت في متغير بيئة اسمه PORT
+    port = int(os.environ.get("PORT", 8000)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
